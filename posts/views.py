1c94d2f81e71652c2a9bd3b3f847942d45b06d4c
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-10-01 22:34:08 +0600

    Initial commit

---
+++ posts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+
+# Create your views here.
+posts =[
+    {
+        'id': 1,
+        'title': 'Chunghua Aluminium',
+        'content': 'Chunghua Aluminium is located in Dhaka, Bangladesh. They specialize in the production of high-quality aluminium profiles, used for doors, windows, and various industrial applications. The company is known for its durable and reliable products in the construction sector.'
+    },
+    {
+        'id': 2,
+        'title': 'Dhaka Thai Aluminium',
+        'content': 'Dhaka Thai Aluminium is one of the leading aluminium companies in Bangladesh, based in Gazipur. They manufacture a wide range of aluminium products including profiles for doors, windows, and curtain walls. The company is a key supplier to both domestic and international markets.'
+    },
+    {
+        'id': 3,
+        'title': 'Alco Aluminium',
+        'content': 'Alco Aluminium is situated in Chittagong, Bangladesh. Their product line includes aluminium sheets, extrusions, and coils, which are used in the construction, automotive, and packaging industries. Alco is known for its innovative solutions and sustainable practices.'
+    },
+]
+def home(request):
+    html=""
+    for post in posts:
+        html += f'''
+            <div>
+                <h1>{post['id']} - {post['title']}</h1>
+                <p> {post['content']}</p>
+            
+            </div>
+        
+        '''
+    # return HttpResponse("<h1>Hello World!</h1>")
+    return HttpResponse(html)
