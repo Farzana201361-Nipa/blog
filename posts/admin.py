444b2699f64169b028f4b897e7d587ff85e3639c
Farzana Akter Nipa <nipa20farzana@gmail.com>
2024-11-04 23:14:01 +0600

    Blog_post are showing in django admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Author,Blog_post
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Blog_post)
